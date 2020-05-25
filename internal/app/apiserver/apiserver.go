72f1210e3ddededb86ff70fd4c2113859cc6255c
Petr <petrakliy@gmail.com>
2020-05-26 00:41:29 +0300

    added configuration for apiserver

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,11 +2,13 @@ package apiserver //here apiserver package
 
 // APIServer ...
 type APIServer struct {
-
+	config *Config
 }
 // New returns already configure instance of APIServer
-func New() *APIServer {
-return &APIServer{}
+func New(config *Config) *APIServer {
+return &APIServer{
+	config: config,
+}
 }
 
 // Start strarts http server Bd and etc
---
+++ internal/app/apiserver/config.go
@@ -0,0 +1,11 @@
+package apiserver
+
+type Config struct {
+	BindAddr string `toml:"bind_addr"` //address where a server is started
+}
+// NewConfig returns default config
+func NewConfig() *Config  {
+	return &Config{
+		BindAddr: ":8080",
+	}
+}
