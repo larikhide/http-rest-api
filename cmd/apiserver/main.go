79b9e72407f0b1a3b81bb9135634ed5ee11c2133
Petr <petrakliy@gmail.com>
2020-05-26 00:23:59 +0300

    added start of server in main.go

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/larikhide/http-rest-api/internal/app/apiserver"
+	"log"
+)
 
+func main() {
+ s := apiserver.New() //starts server
+ if err := s.Start(); err != nil {
+ 	log.Fatal(err)
+ }
 }
