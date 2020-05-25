1799a110df74b0201363e8e65b61510ae9eb4c25
Petr <petrakliy@gmail.com>
2020-05-26 00:52:04 +0300

    added parser from toml into config file

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,14 +4,15 @@ package apiserver //here apiserver package
 type APIServer struct {
 	config *Config
 }
+
 // New returns already configure instance of APIServer
 func New(config *Config) *APIServer {
-return &APIServer{
-	config: config,
-}
+	return &APIServer{
+		config: config,
+	}
 }
 
 // Start strarts http server Bd and etc
-func (s *APIServer) Start() error  {
+func (s *APIServer) Start() error {
 	return nil
 }
