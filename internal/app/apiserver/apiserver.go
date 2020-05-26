f0125a066640a1345a3b8b3172d8d8520b44a602
Petr <petrakliy@gmail.com>
2020-05-26 15:53:33 +0300

    added router

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,13 +1,17 @@
 package apiserver //here apiserver package
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
 )
 
 // APIServer ...
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New returns already configure instance of APIServer
@@ -15,6 +19,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -24,8 +29,10 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter()
+
 	s.logger.Info("starting api server")
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -35,4 +42,21 @@ func (s *APIServer) configureLogger() error {
 	}
 	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (a *APIServer) handleHello() http.HandlerFunc {
+	// allow to init some variables which use only this handler
+	// and code will execute once
+	// for example
+	/*type request struct {
+		name string
+	}*/
+	return func(w http.ResponseWriter, r *http.Request) {
+		// business logic here
+		io.WriteString(w, "Hello! from web-server?")
+	}
 }
