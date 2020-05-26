bbaceb5ec009876f79ebd108d773cc142b42a3ba
Petr <petrakliy@gmail.com>
2020-05-26 15:13:55 +0300

    added logging

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,38 @@
 package apiserver //here apiserver package
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 // APIServer ...
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 // New returns already configure instance of APIServer
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start strarts http server Bd and etc
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
 	return nil
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,12 +1,14 @@
 package apiserver
 
 type Config struct {
-	BindAddr string `toml:"bind_addr"` //address where a server is started
+	BindAddr string `toml:"bind_addr"`  //address where a server is started
+	LogLevel string `toml:"log_level"` //because logrus support different levels of logging
 }
 
 // NewConfig returns default config
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
+		LogLevel: "debug",
 	}
 }
